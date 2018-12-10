32196b1edd3525d0e4052e46a4b8c3afe15d55a9
huiminwu0 <huiminwu24@gmail.com>
2018-12-09 19:37:54 -0500

    Finish hw

--- main.c
+++ main.c
@@ -6,12 +6,43 @@
 #include <sys/shm.h>
 
 int main() {
-	key_t key = 12345678910;
 	int shmid;
 	
-	if ((shmid = shmget(key, 200, 0644 | IPC_CREAT)) == -1) {
+	if ((shmid = shmget(0, 200, 0644 | IPC_CREAT)) == -1) {
 		perror("shmget");
 		exit(1);
 	}
+    printf("Shared mem seg created at %d\n", shmid);
+
+    char * data;
+    data = shmat(shmid, (void *)0, 0);
+    if (data) {
+        printf("just created\n");
+    } else {
+        printf("contents: %s\n", data);
+    }
+    
+    char input[100];
+    printf("Do you want to change the data in the segment? (y/n)\n");
+    fgets(input, 200, stdin);
+    if(input[0] == 'y') {  
+        printf("What would u want to add? \n");
+        fgets(input, 200, stdin);
+        strcpy(data, input);
+        printf("New data in segment: %s\n", data);
+    } else {
+        printf("Okay, you do not want to change anything\n");
+    }
+
+    printf("Do you want to delete the segment? (y/n)\n");
+    fgets(input, 200, stdin);
+    if(input[0] == 'y') {
+        shmdt(data);
+        shmctl(shmid, IPC_RMID, NULL);
+        printf("Deleted!\n");
+    } else {
+        printf("Okay, you do not want to delete this segment\n");
+    }
+
 	return 0;
 }
